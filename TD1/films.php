a1985a02d04f36202e2fbfa843ffc11474a76d08
afadili <aminafadili@gmail.com>
2018-02-22 20:43:36 +0100

    Ameliorations du code

--- TD1/films.php
+++ TD1/films.php
@@ -6,55 +6,62 @@
 	</head>
 	<body>
 		<h1> Movie List </h1>
-	</body>
-	</html>
-
 <?php
 
 /* fonction render_movie_list */
 
-function render_movie_list ($movies)
+function render_movie_list ($movies, $genre, $annee)
 {
-	$rendu = "<ul>";
-	foreach ($movies as $key) {
-		$rendu.= "<li>";
-		if ($key["year"] >= date('Y')-10) // teste si le film a moins de 10 ans et il le met en gras
-		{
-			$rendu = $rendu.$key["title"]." "."<strong>". "(".$key["year"].")"."</strong>";
-			$rendu.= "</li>";
-		}
-		else // sinon il ne fait rien
-		{
-			$rendu = $rendu.$key["title"]. " ". "(" . $key["year"]. ")";
-			$rendu.= "</li>";
-		}
-		$rendu.= "<ul>";
-		if ($key["genre"] == "Science Fiction") // test si le genre est science fiction et le met en bleu
-		{
+	if (empty($movies) != true)
+	{
+			$rendu = "<ul>";
+		foreach ($movies as $key) {
 			$rendu.= "<li>";
-			$rendu = $rendu."<span style = 'color : blue'>"."Genre :".$key["genre"]."</span>";
-			$rendu.= "</li>";
-			$rendu.= "<li>";
-			$rendu = $rendu."Director :".$key["director"];
-			$rendu.= "</li>";
+			if ($key["year"] >= date('Y')-$annee) // teste si le film a moins de $annee ans et il le met en gras
+			{
+				$rendu.= $key["title"]." "."<strong>". "(".$key["year"].")"."</strong>";
+				$rendu.= "</li>";
+			}
+			else // sinon il ne fait rien
+			{
+				$rendu.= $key["title"]. " ". "(" . $key["year"]. ")";
+				$rendu.= "</li>";
+			}
+			$rendu.= "<ul>";
+			if ($key["genre"] == $genre ) // test si le genre est ce qu'on veut et le met en bleu
+			{
+				$rendu.= "<li>";
+				$rendu.= "<span style = 'color : blue'>"."Genre :".$key["genre"]."</span>";
+				$rendu.= "</li>";
+				$rendu.= "<li>";
+				$rendu.= "Director :".$key["director"];
+				$rendu.= "</li>";
+			}
+			else // sinon il ne fait rien
+			{
+				$rendu.= "<li>";
+				$rendu.= "Genre : ".$key["genre"];
+				$rendu.= "</li>";
+				$rendu.= "<li>";
+				$rendu.= "Director : ".$key["director"];
+				$rendu.= "</li>";
+			}
+			$rendu.= "</ul>";
+			
 		}
-		else // sinon il ne fait rien
-		{
-			$rendu.= "<li>";
-			$rendu = $rendu."Genre : ".$key["genre"];
-			$rendu.= "</li>";
-			$rendu.= "<li>";
-			$rendu = $rendu."Director : ".$key["director"];
-			$rendu.= "</li>";
-		}
-		$rendu.= "</ul>";
-		
+		$rendu.="</ul>";
+		return $rendu;
 	}
-	$rendu.="</ul>";
-	return $rendu;
+	else 
+	{
+		$rendu = "Pas de films disponible pour le moment ... Oups !";
+		return $rendu ;
+	}	
 }
 
 require "data.movies.php";
-echo render_movie_list($movies);
+echo render_movie_list($movies, "Science Fiction", 10);
 
 ?>
+</body>
+</html>
